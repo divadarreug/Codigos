f2ae4242a74acaa1ee02e7e9437d03fdd19193be
Wartz <divadarreug98@gmail.com>
2023-02-22 09:36:01 -0500

    Update nombres.py

--- nombres.py
+++ nombres.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-ruta_carpeta = r"\\172.20.136.226\Respaldos\2022"
+ruta_carpeta = r"\\172.XXX.XXX.XXX\" #(Nombre de la carpeta )
 
 nombres_archivos = os.listdir(ruta_carpeta)
 
@@ -9,5 +9,5 @@ nombres_archivos = os.listdir(ruta_carpeta)
 df_archivos = pd.DataFrame(nombres_archivos, columns=["Nombre del archivo"])
 
 
-nombre_archivo_excel = "lista_archivos_2022.xlsx"
+nombre_archivo_excel = "lista_archivos_2022.xlsx" #(Nombre del archivo excel)
 df_archivos.to_excel(nombre_archivo_excel, index=False)
